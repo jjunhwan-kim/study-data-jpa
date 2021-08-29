6cbb21f35be4e3aaaf2a759acbd43a428cd7cb84
nefertirii <nefertiri972@gmail.com>
2021-08-29 20:51:56 +0900

    Add entity graph
    - 연관된 엔티티들을 SQL 한 번에 조회하는 방법 추가
      - JPQL 페치 조인 사용
      - @EntityGraph 사용
        - 공통 메서드 오버라이드
        - JPQL + 엔티티 그래프
        - 메서드 이름으로 쿼리 생성
    - @NamedEntityGraph를 엔티티에 정의하고 스프링 데이터 JPA 리포지토리 메서드에서 @EntityGraph로 불러올 수 있음

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -265,8 +265,41 @@ class MemberRepositoryTest {
         Member member = result.get(0);
         assertThat(member.getAge()).isEqualTo(41);
         System.out.println("member = " + member);
-        
+
         // then
         assertThat(resultCount).isEqualTo(3);
+    }
+
+    @Test
+    void findMemberLazy() {
+        // given
+        // member1 -> teamA
+        // member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> members = memberRepository.findAll();
+        System.out.println("==========================");
+        List<Member> members2 = memberRepository.findMemberFetchJoin();
+        List<Member> members3 = memberRepository.findMemberEntityGraph();
+        List<Member> members4 = memberRepository.findEntityGraphByUsername("member1");
+
+        for (Member member : members) {
+            System.out.println("member.getUsername() = " + member.getUsername());
+            System.out.println("member.getTeam().getClass() = " + member.getTeam().getClass());
+            System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
+        }
     }
 }
